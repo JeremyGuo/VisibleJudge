bf66374466103e62b4997a27d33ed2a6edc01ba5
JeremyGuo <915119769@qq.com>
2017-01-27 15:23:03 +0800

    Add more form

---
+++ C#/JudgeMini/JudgeMini/JudgeDiag.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JeremyGuo
+ * Date: 2017/1/27
+ * Time: 15:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace JudgeMini
+{
+	/// <summary>
+	/// Description of JudgeDiag.
+	/// </summary>
+	public partial class JudgeDiag : Form
+	{
+		public JudgeDiag()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
--- C#/JudgeMini/JudgeMini/MainForm.Designer.cs
+++ C#/JudgeMini/JudgeMini/MainForm.Designer.cs
@@ -14,6 +14,14 @@ namespace JudgeMini
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -32,16 +40,117 @@ namespace JudgeMini
 		/// <summary>
 		/// This method is required for Windows Forms designer support.
 		/// Do not change the method contents inside the source code editor. The Forms designer might
-		/// not be able to load this method if it was changed manually.
-		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.button1.Location = new System.Drawing.Point(12, 283);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(228, 44);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Judge";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label1.Location = new System.Drawing.Point(6, 37);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 24);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "程序名";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.groupBox1.Location = new System.Drawing.Point(13, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(227, 265);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "评测信息";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.textBox2.Location = new System.Drawing.Point(80, 69);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(141, 26);
+			this.textBox2.TabIndex = 6;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.textBox1.Location = new System.Drawing.Point(80, 35);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(141, 26);
+			this.textBox1.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label4.Location = new System.Drawing.Point(6, 139);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(68, 24);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "label4";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label3.Location = new System.Drawing.Point(6, 105);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(68, 24);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "label3";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label2.Location = new System.Drawing.Point(6, 71);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(68, 24);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "label2";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "JudgeMini";
+			this.ClientSize = new System.Drawing.Size(252, 339);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
 			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "JudgeMini";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
 		}
 	}
 }
